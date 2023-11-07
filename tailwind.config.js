c56ee73a2e765066f2c9e212497a8cf30ed18c96
nguyen452 <nguyen452@gmail.com>
2023-11-06 22:26:30 -0800

    make navbar, login page, sign up page responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'traderBlue': '#2f70f2'
+        'traderBlue': '#2f70f2',
+        'backgroudGrey': '#F5F6FA',
+        'softRed': '#EF627A',
+        'softGreen': '#00C49F',
       },
       width: {
-        '500px': '500px'
+        '500px': '500px',
+        '420px': '420px'
       },
       height: {
-        '68px': '68px'
+        '70px': '70px',
+        '768px': '768px'
       },
     },
   },
