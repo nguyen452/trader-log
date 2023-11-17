8d01e7a55ed44fb40fd4f798df39736d5f1be622
nguyen452 <nguyen452@gmail.com>
2023-11-16 18:35:05 -0800

    add welcome bar

---
+++ src/components/WelcomeBar.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ButtonWithDropDownMenu from "./common/ButtonWithDropDownMenu";
+import { useSelector } from "react-redux";
+import { selectSelectedPeriod, changePeriod } from "../slice/periodSlice";
+
+const WelcomeBar = () => {
+    const selectedPeriod = useSelector(selectSelectedPeriod);
+
+    const getPeriodText = () => {
+        switch(selectedPeriod){
+            case 'All time': return 'all time';
+            case 'Year to date': return 'the year to date';
+            case 'Last 30 days': return 'the last 30 days';
+            case 'Last 7 days': return 'the last 7 days';
+            default: return ''; // Default case if needed
+        }
+    }
+
+    return (
+        <div className="flex h-24 items-center container mx-auto p-4 text-3xl font-semibold text-slate-900 justify-between mt-8">
+            <div className="flex flex-col space-y-4">
+                <h1>{`Welcome, User Name!`}</h1>
+                <h2 className="text-base text-slate-600">{`Here are your trading metrics for ${
+                    getPeriodText()
+                    }`}</h2>
+            </div>
+            <div>
+                <ButtonWithDropDownMenu
+                    name={selectedPeriod}
+                    list={[
+                        "All time",
+                        "Year to date",
+                        "Last 30 days",
+                        "Last 7 days",
+                    ]}
+                    action={changePeriod}
+                />
+            </div>
+        </div>
+    );
+};
+export default WelcomeBar;
