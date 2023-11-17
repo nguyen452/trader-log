ae2b28bae84faa8e6ce385bc4cc836137323ac20
nguyen452 <nguyen452@gmail.com>
2023-11-16 18:32:54 -0800

    add calendar, period recuers

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slice/authSlice'
 import signUpReducer from '../slice/signUpSlice'
+import periodReducer from '../slice/periodSlice'
+import calendarReducer from '../slice/calendarSlice';
 
 export const store = configureStore({
   reducer: {
     authenticate: authReducer,
-    createUser: signUpReducer
+    calendar: calendarReducer,
+    createUser: signUpReducer,
+    period: periodReducer,
   },
 });
