12c7c28032a761184f1b2b79d22735631b9d200a
nguyen452 <nguyen452@gmail.com>
2024-02-23 16:34:09 -0800

    Refactor ProfitsMiniAreaChart component to include prop for y & x axis and cartesian grid

--- src/components/ProfitsMiniAreaChart.js
+++ src/components/ProfitsMiniAreaChart.js
@@ -18,33 +18,34 @@ const chartStyle = {
     padding: "8px",
 };
 
-const ProfitMiniAreaChart = ({ data }) => {
+const ProfitMiniAreaChart = ({ data, yAxis, xAxis, cartesianGrid }) => {
     return (
+        <ResponsiveContainer width={192} height={96}>
+            <AreaChart
+                data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+            >
+                <defs>
+                    <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
+                        <stop offset="5%" stopColor="#1083EF" stopOpacity={0.8} />
+                        <stop offset="95%" stopColor="#1083EF" stopOpacity={0.0} />
+                    </linearGradient>
+                </defs>
 
-            <ResponsiveContainer width={192} height={96}>
-                <AreaChart
-                    data={data}
-                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-                >
-                    <defs>
-                        <linearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
-                            <stop offset="5%" stopColor="#1083EF" stopOpacity={0.8} />
-                            <stop offset="95%" stopColor="#1083EF" stopOpacity={0.0} />
-                        </linearGradient>
-                    </defs>
+                {yAxis && <YAxis />}
+                {xAxis && <XAxis />}
+                {cartesianGrid && <CartesianGrid strokeDasharray="3 3" />}
 
-
-                    <Area
-                        type="monotone"
-                        dataKey="Accumulated Profits"
-                        stroke="url(#gradient)"
-                        strokeWidth={2}
-                        dot={false}
-                        fill="url(#gradient)"
-                    />
-                </AreaChart>
-            </ResponsiveContainer>
-
+                <Area
+                    type="monotone"
+                    dataKey="Accumulated Profits"
+                    stroke="url(#gradient)"
+                    strokeWidth={2}
+                    dot={false}
+                    fill="url(#gradient)"
+                />
+            </AreaChart>
+        </ResponsiveContainer>
     );
 };
 export default ProfitMiniAreaChart;
