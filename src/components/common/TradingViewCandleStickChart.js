e91bee45e3cb386c8d4284996a3907fae7e0b6c0
nguyen452 <nguyen452@gmail.com>
2024-03-22 05:53:08 -0700

    Refactor TradingViewCandleStickChart component to improve chart options and volume color

--- src/components/common/TradingViewCandleStickChart.js
+++ src/components/common/TradingViewCandleStickChart.js
@@ -1,28 +1,49 @@
 import React, { useEffect, useRef } from "react";
 import { createChart } from "lightweight-charts";
 
-const TradingViewCandleStickChart = ({data}) => {
+const TradingViewCandleStickChart = ({ data }) => {
     const chartContainerRef = useRef();
     useEffect(() => {
         const chart = createChart(chartContainerRef.current, {
             width: chartContainerRef.current.clientWidth,
             height: chartContainerRef.current.clientHeight,
         });
+        chart.timeScale().applyOptions({
+            timeVisible: true,
+            secondsVisible: false,
+        });
+
+        chart.applyOptions({
+            rightPriceScale: {
+                scaleMargins: {top: 0, bottom: 0.25}
+            },
+        })
         const candlestickSeries = chart.addCandlestickSeries();
 
-        const volume = data.map(item => {return {time: item.time, value: item.volume}});
+        const volume = data.map((item) => {
+            if (Number(item.open) >= Number(item.close)) {
+                return { time: item.time, value: item.volume };
+            } else {
+                return {
+                    time: item.time,
+                    value: item.volume,
+                    color: "rgba(239, 83, 80, 0.75)",
+                };
+            }
+        });
 
         const volumeSeries = chart.addHistogramSeries({
-            color: "#26a69a",
+            color: "rgba(38, 166, 154, 0.75)",
             lineWidth: 2,
             priceFormat: {
                 type: "volume",
             },
-            priceScaleId: '',
-
+            priceScaleId: "",
         });
-        volumeSeries.priceScale().applyOptions({scaleMargins:{top: 0.8, bottom: 0}});
-        candlestickSeries.priceScale().applyOptions({top: 0.1, bottom: 0.4})
+        volumeSeries
+            .priceScale()
+            .applyOptions({ scaleMargins: { top: 0.8, bottom: 0 } });
+        candlestickSeries.priceScale().applyOptions({ top: 0.6, bottom: 0.4 });
         volumeSeries.setData(volume);
         candlestickSeries.setData(data);
         chart.timeScale().fitContent();
@@ -35,8 +56,6 @@ const TradingViewCandleStickChart = ({data}) => {
     } else if (data.length === 0) {
         return <div>No data available</div>;
     }
-
-
 
     return <div ref={chartContainerRef} className="h-full w-full"></div>;
 };
