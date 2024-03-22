621200e7d294a5e389040b0799aaae3587a316e7
nguyen452 <nguyen452@gmail.com>
2024-03-22 05:53:50 -0700

    Refactor fetchTradeData to include tradingViewData

--- src/slice/tradesSlice.js
+++ src/slice/tradesSlice.js
@@ -2,42 +2,60 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchTradeData = createAsyncThunk(
     'trades/fetchTradeData',
-    async (id) => {
+    async (id, thunkAPI) => {
+        let tradeData;
+        let tradingViewData;
+
         const response = await fetch(`http://localhost:4000/api/trades/${id}`, {
             method: "GET",
             credentials: "include"
         });
         if (response.ok) {
-            const tradeData = await response.json();
-            return tradeData;
+            tradeData = await response.json();
         } else {
             throw new Error('Unable to fetch trade data');
         }
-    }
-)
 
-export const fetchTradingViewData = createAsyncThunk(
-    'trades/fetchTradingViewData',
-    async ({startDate, endDate, timeframe, symbol}) => {
-        const response = await fetch(`http://localhost:4000/api/marketData/?symbol=${symbol}&startDate=${startDate}&endDate=${endDate}&timeframe=${timeframe}`, {
+        const symbol = tradeData.trade.symbol;
+        const startDate = tradeData.trade.date_open;
+        const endDate = tradeData.trade.date_close;
+        const timeframe = "1min";
+
+        const response1 = await fetch(`http://localhost:4000/api/marketData/?symbol=${symbol}&startDate=${startDate}&endDate=${endDate}&timeframe=${timeframe}`, {
             method: "GET",
             credentials: "include"
         });
-        if (response.ok) {
-            const tradingViewData = await response.json();
-            return tradingViewData;
+        if (response1.ok) {
+            tradingViewData = await response1.json();
         } else {
             throw new Error('Unable to fetch trading view data');
         }
+        return {tradeData, tradingViewData};
     }
-
 )
+
+// export const fetchTradingViewData = createAsyncThunk(
+//     'trades/fetchTradingViewData',
+//     async ({startDate, endDate, timeframe, symbol}) => {
+//         const response = await fetch(`http://localhost:4000/api/marketData/?symbol=${symbol}&startDate=${startDate}&endDate=${endDate}&timeframe=${timeframe}`, {
+//             method: "GET",
+//             credentials: "include"
+//         });
+//         if (response.ok) {
+//             const tradingViewData = await response.json();
+//             return tradingViewData;
+//         } else {
+//             throw new Error('Unable to fetch trading view data');
+//         }
+//     }
+
+// )
 const tradesSlice = createSlice({
     name: 'trades',
     initialState: {
         isLoading: false,
         hasError: false,
-        tradeData: [],
+        tradeData: {trade: {date_close: "", symbol: "", profit: 0}, totalSharesTraded: 0, executions: [{}]},
         tradingViewData:[],
     },
     reducers: {},
@@ -49,7 +67,8 @@ const tradesSlice = createSlice({
         builder.addCase(fetchTradeData.fulfilled, (state, action) => {
             state.isLoading = false;
             state.hasError = false;
-            state.tradeData = action.payload;
+            state.tradeData = action.payload.tradeData;
+            state.tradingViewData = action.payload.tradingViewData;
         });
         builder.addCase(fetchTradeData.rejected, (state) => {
             state.isLoading = false;
@@ -62,3 +81,4 @@ export default tradesSlice.reducer;
 export const selectTradeData = (state) => state.trades.tradeData;
 export const selectTradeDataLoading = (state) => state.trades.isLoading;
 export const selectTradeDataError = (state) => state.trades.hasError;
+export const selectTradingViewData = (state) => state.trades.tradingViewData;
